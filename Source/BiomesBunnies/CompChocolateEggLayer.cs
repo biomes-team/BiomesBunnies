104fa0ae96506a7fe3fc29744aa42e1376e2fb69
Zylleon <56230821+Zylleon@users.noreply.github.com>
2021-03-19 14:51:27 -0600

    Initial commit

---
+++ Source/BiomesBunnies/CompChocolateEggLayer.cs
@@ -0,0 +1,93 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace BiomesBunnies
+{
+	public class CompChocolateEggLayer : ThingComp
+	{
+		private float eggProgress;
+
+		private bool Active
+		{
+			get
+			{
+				Pawn pawn = parent as Pawn;
+
+				if (pawn != null && !pawn.ageTracker.CurLifeStage.milkable)
+				{
+					return false;
+				}
+				return true;
+			}
+		}
+
+		public CompProperties_ChocolateEggLayer Props => (CompProperties_ChocolateEggLayer)props;
+
+		public override void PostExposeData()
+		{
+			base.PostExposeData();
+			Scribe_Values.Look(ref eggProgress, "eggProgress", 0f);
+		}
+
+		public override void CompTick()
+		{
+			if (Active)
+			{
+				float num = 1f / (Props.eggLayIntervalDays * 60000f);
+				Pawn pawn = parent as Pawn;
+				if (pawn != null)
+				{
+					num *= PawnUtility.BodyResourceGrowthSpeed(pawn);
+				}
+				eggProgress += num;
+				if (eggProgress > 1f)
+				{
+					eggProgress = 1f;
+					if (pawn.Awake() && !pawn.Downed)
+                    {
+						ProduceEgg(pawn);
+						eggProgress = 0f;
+                    }
+				}
+			}
+		}
+
+
+		public void ProduceEgg(Pawn pawn)
+		{
+			if (!Active)
+			{
+				Log.Error("LayEgg while not Active: " + parent);
+			}
+			eggProgress = 0f;
+			Thing egg;
+			int randomInRange = Props.eggCountRange.RandomInRange;
+			if (randomInRange == 0)
+			{
+				egg = null;
+			}
+
+			egg = ThingMaker.MakeThing(Props.eggDef);
+			egg.stackCount = randomInRange;
+
+			GenSpawn.Spawn(egg, pawn.Position, pawn.Map).SetForbiddenIfOutsideHomeArea();
+		}
+
+		public override string CompInspectStringExtra()
+		{
+			if (!Active)
+			{
+				return null;
+			}
+			string text = "EggProgress".Translate() + ": " + eggProgress.ToStringPercent();
+
+			return text;
+		}
+	}
+}
---
+++ Source/BiomesBunnies/CompProperties_ChocolateEggLayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BiomesBunnies
+{
+	public class CompProperties_ChocolateEggLayer : CompProperties
+	{
+		public float eggLayIntervalDays = 5f;
+
+		public IntRange eggCountRange = IntRange.one;
+
+		public ThingDef eggDef;
+
+		//public ThingDef eggFertilizedDef;
+
+		//public int eggCountMax = 1;
+
+		//public bool eggLayFemaleOnly = false;
+
+		//public float eggProgressUnfertilizedMax = 1f;
+
+		public CompProperties_ChocolateEggLayer()
+		{
+			compClass = typeof(CompChocolateEggLayer);
+		}
+	}
+}
